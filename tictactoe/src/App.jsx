ea43979828643fe58edb4802c455e14c18390669
Saksham Kashyap <kashyapsaksham012@gmail.com>
2025-06-29 00:34:02 +0530

    Add Tic Tac Toe project

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,17 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TicTacToe from './Components/TicTacToe'
+
+
+function App() {
+
+  return (
+    <>
+    <TicTacToe/>
+
+    </>
+  )
+}
+
+export default App
